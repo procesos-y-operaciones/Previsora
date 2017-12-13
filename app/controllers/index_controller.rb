4f43f63e4d31b6b178db612f81b2c6950d61bcc0
joagranadosme <joagranadosme@unal.edu.co>
2017-12-13 14:47:48 -0500

    .

--- app/controllers/index_controller.rb
+++ app/controllers/index_controller.rb
@@ -3,10 +3,11 @@ class IndexController < ApplicationController
   def home
     @user = current_user
     if @user.role == 1
-      redirect_to
+      redirect_to index_lawyer_path
     elsif @user.role == 2
+      redirect_to index_coordinator_path
     elsif @user.role == 3
-    else
+      redirect_to index_administrator_path
     end
   end
 
