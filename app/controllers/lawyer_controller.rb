ebac4f90d5232478c79e68c8a4cf6e5a7937945e
joagranadosme <joagranadosme@unal.edu.co>
2018-01-15 16:03:56 -0500

    .

--- app/controllers/lawyer_controller.rb
+++ app/controllers/lawyer_controller.rb
@@ -18,15 +18,29 @@ class LawyerController < ApplicationController
     @search = current_user.type_processes.ransack(params[:q])
     @report = @search.result
     @processes = @search.result.paginate(page: params[:page], per_page: 10)
+
     if params[:page] == nil
       @page = 0
     else
       @page = 10 * (params[:page].to_i - 1)
     end
+
+    if params[:q] == nil || params[:q]["creation_date_gteq"] == nil
+      @date_from = "01-01-1980"
+    else
+      @date_from = params[:q]["creation_date_gteq"]
+    end
+
+    if params[:q] == nil || params[:q]["creation_date_lteq"] == nil
+      @date_till = Date.today
+    else
+      @date_till = params[:q]["creation_date_lteq"]
+    end
+
     respond_to do |format|
       format.html
       format.csv { send_data @report.to_csv }
-      format.xls { send_data @report.to_csv(params[:q]["creation_date_gteq"], params[:q]["creation_date_lteq"], col_sep: "\t") }
+      format.xls { send_data @report.to_csv(@date_from, @date_till, col_sep: "\t") }
     end
   end
 
