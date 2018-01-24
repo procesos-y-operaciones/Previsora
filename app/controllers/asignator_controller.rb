4c133da4960a8ed1bd2fbe86c527ae7728e33915
joagranadosme <joagranadosme@unal.edu.co>
2018-01-23 21:23:03 -0500

    Finishing view asignate

--- app/controllers/asignator_controller.rb
+++ app/controllers/asignator_controller.rb
@@ -14,7 +14,14 @@ class AsignatorController < ApplicationController
   end
 
   def asignate
-    @processes = TypeProcess.all
+    @search = TypeProcess.ransack(params[:q])
+    @report = @search.result
+    @processes = @search.result.paginate(page: params[:page], per_page: 10)
+    if params[:page] == nil
+      @page = 0
+    else
+      @page = 10 * (params[:page].to_i - 1)
+    end
   end
 
   def translate
