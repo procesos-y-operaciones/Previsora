dce6dfbb912da68ab9df55c2a734f482d181180f
Jonathan Granados <joagranadosme@unal.edu.co>
2018-03-05 17:54:58 +0000

    Fix report name

--- app/models/type_process.rb
+++ app/models/type_process.rb
@@ -240,7 +240,7 @@ class TypeProcess < ApplicationRecord
     elsif self.branch_commercial == "0"
       "PENDIENTE"
     else
-       BranchCommercial.where(num: self.branch_commercial).name
+       BranchCommercial.where(num: self.branch_commercial)[0].name
     end
   end
 
@@ -346,7 +346,7 @@ class TypeProcess < ApplicationRecord
     elsif self.branch_policy == "0"
       "PENDIENTE"
     else
-      BranchPolicy.where(num: self.branch_policy).name
+      BranchPolicy.where(num: self.branch_policy)[0].name
     end
   end
 
