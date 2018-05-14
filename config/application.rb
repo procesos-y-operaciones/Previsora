57a06ed4a8e4e31580d77388f8827dc2db8086c4
joagranadosme <joagranadosme@unal.edu.co>
2018-05-14 17:31:02 -0500

    Login fixed

--- config/application.rb
+++ config/application.rb
@@ -13,6 +13,7 @@ module Previsora
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
+    # config.force_ssl = true
     config.secret_key_base = 'Previsora2018'
   end
 end
