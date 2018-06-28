baff4d1628534f3df6412de6bea966cb2a662bdc
joagranadosme <joagranadosme@unal.edu.co>
2018-06-28 18:21:18 -0500

    .

--- db/migrate/20180614212321_add_booleans_to_typeprocess.rb
+++ db/migrate/20180614212321_add_booleans_to_typeprocess.rb
@@ -6,5 +6,8 @@ class AddBooleansToTypeprocess < ActiveRecord::Migration[5.1]
     add_column :type_processes, :has_coactive_radicate, :boolean, default: false
     add_column :type_processes, :has_impug, :boolean, default: false
     add_column :type_processes, :has_desacate, :boolean, default: false
+    add_column :type_processes, :state, :string, default: "REGISTRO NUEVO"
+    add_column :type_processes, :document_active_part, :string, default: "NO APLICA"
+    add_column :type_processes, :document_passive_part, :string, default: "NO APLICA"
   end
 end
--- db/migrate/20180622223227_add_document_to_type_processes.rb
+++
@@ -1,7 +0,0 @@
-class AddDocumentToTypeProcesses < ActiveRecord::Migration[5.1]
-  def change
-    add_column :type_processes, :state, :string, default: "REGISTRO NUEVO"
-    add_column :type_processes, :document_active_part, :string, default: "NO APLICA"
-    add_column :type_processes, :document_passive_part, :string, default: "NO APLICA"
-  end
-end
