a535ac58a7b7c3ee2e83a38f460d0142fda96a23
joagranadosme <joagranadosme@unal.edu.co>
2018-07-04 14:53:54 -0500

    Adding office service

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -11,6 +11,16 @@ class ApplicationController < ActionController::Base
     render json: CityCase.select(:id, :code, :name).order(name: :asc).where(departament: params[:state]).to_json
   end
 
+  def offices
+    #render json: params[:name].split(",").to_json
+    @parameters = params[:name].split(",")
+    #render json: @parameters.to_json
+    render json: OfficeName.select(:id, :name).order(name: :asc).where(
+      "name like ? and name like ? and name like ? or name like ? and name like ? and name like ?",
+      "%#{@parameters[0]}%", "%#{@parameters[2]}%", "%#{@parameters[3]}%", "%#{@parameters[1]}%", "%#{@parameters[2]}%", "%#{@parameters[3]}%",
+    ).to_json
+  end
+
   protected
     def configure_permitted_parameters
       devise_parameter_sanitizer.permit(:sign_up, keys: [:document])
