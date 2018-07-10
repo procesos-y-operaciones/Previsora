f695757fff8e50438e9aba4cf2278fc677d277ab
joagranadosme <joagranadosme@unal.edu.co>
2018-07-10 08:37:34 -0500

    .

--- db/migrate/20180706145953_add_fieds_to_departamentes_cities.rb
+++ db/migrate/20180706145953_add_fieds_to_departamentes_cities.rb
@@ -6,6 +6,9 @@ class AddFiedsToDepartamentesCities < ActiveRecord::Migration[5.1]
     add_column :city_cases, :code, :string
     add_column :city_cases, :departament, :string
 
+    change_column_default(:type_processes, :departament, "00")
+    change_column_default(:type_processes, :city_case, "00")
+
   end
 
 end
