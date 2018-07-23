8e35e7b89c588335377bc461f4f04120bc7d77c5
joagranadosme <joagranadosme@unal.edu.co>
2018-07-23 11:29:28 -0500

    Updating attornies

--- app/models/attorny.rb
+++ app/models/attorny.rb
@@ -1,2 +1,7 @@
 class Attorny < ApplicationRecord
+
+  def self.get_all
+    Attorny.order(:name).where.not(id: 0)
+  end
+
 end
---
+++ db/migrate/20180723162759_updating.rb
@@ -0,0 +1,4 @@
+class Updating < ActiveRecord::Migration[5.1]
+  def change
+  end
+end
