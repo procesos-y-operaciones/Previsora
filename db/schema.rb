299c82fb945fffc1978dd1e62ec506203e523d77
joagranadosme <joagranadosme@unal.edu.co>
2018-07-24 18:54:43 -0500

    .

--- db/schema.rb
+++ db/schema.rb
@@ -10,11 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-<<<<<<< HEAD
 ActiveRecord::Schema.define(version: 20180718001638) do
-=======
-ActiveRecord::Schema.define(version: 20180714020919) do
->>>>>>> ReportFix
 
   create_table "branch_commercials", force: :cascade do |t|
     t.string "name"
