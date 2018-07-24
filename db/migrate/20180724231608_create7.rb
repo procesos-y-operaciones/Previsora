2427cf3d6ad2ec8cb693de04652a440500b5c7e2
joagranadosme <joagranadosme@unal.edu.co>
2018-07-24 18:16:45 -0500

    Create7

---
+++ db/migrate/20180724231608_create7.rb
@@ -0,0 +1,45 @@
+class Create7 < ActiveRecord::Migration[5.1]
+  def change
+TypeProcess.create(p_type: 2,process_radicate: '20130002600',case_id_bap: 'GPJ-1721-2016',departament: '11',city_case: '001',passive_part: ' ',instance: ' ',detritment_cents: 'PENDIENTE',fail_value_cents: 'PENDIENTE',office_name: 'JUZGADO 4 ADMINISTRATIVO ORALES DE BOGOTA',internal_lawyer: '1',state: 'REGISTRO MIGRADO')
+
+TypeProcess.create(p_type: 2,process_radicate: '20130046000',case_id_bap: 'GPJ-2486-2016',departament: '11',city_case: '001',passive_part: ' ',instance: ' ',detritment_cents: 'PENDIENTE',fail_value_cents: 'PENDIENTE',office_name: 'JUZGADO 8 ADMINISTRATIVO ORALES DE BOGOTA',internal_lawyer: '1',state: 'REGISTRO MIGRADO')
+
+TypeProcess.create(p_type: 2,process_radicate: '05001333102120090023900',case_id_bap: 'GPJ-2518-2016',departament: '05',city_case: '001',passive_part: ' ',instance: ' ',detritment_cents: 'PENDIENTE',fail_value_cents: 'PENDIENTE',office_name: 'JUZGADO 3 ADMINISTRATIVO CIRCUITO DE MEDELLIN',internal_lawyer: '1',state: 'REGISTRO MIGRADO')
+
+TypeProcess.create(p_type: 2,process_radicate: '20150016000',case_id_bap: 'GPJ-2673-2016',departament: '11',city_case: '001',passive_part: ' ',instance: ' ',detritment_cents: 'PENDIENTE',fail_value_cents: 'PENDIENTE',office_name: 'JUZGADO 6 ADMINISTRATIVO ORALES DE BOGOTA',internal_lawyer: '1',state: 'REGISTRO MIGRADO')
+
+TypeProcess.create(p_type: 2,process_radicate: '05001333102620010011800',case_id_bap: 'GPJ-2677-2016',departament: '05',city_case: '001',passive_part: 'E.S.E. METROSALUD',instance: 'PRIMERA',detritment_cents: '113400000.0',fail_value_cents: '0.0',office_name: 'JUZGADO 26 ADMINISTRATIVO CIRCUITO DE MEDELLIN',internal_lawyer: '1',state: 'REGISTRO MIGRADO')
+
+TypeProcess.create(p_type: 2,process_radicate: '05001333100620080005600',case_id_bap: 'GPJ-2679-2016',departament: '05',city_case: '001',passive_part: 'ESE METROSALUD',instance: 'SEGUNDA',detritment_cents: '196332879.0',fail_value_cents: '196339287.0',office_name: 'JUZGADO 6 ADMINISTRATIVO CIRCUITO DE MEDELLIN',internal_lawyer: '1',state: 'REGISTRO MIGRADO')
+
+TypeProcess.create(p_type: 2,process_radicate: '05001333100120070023800',case_id_bap: 'GPJ-2680-2016',departament: '05',city_case: '001',passive_part: 'E.S.E. METROSALUD',instance: 'SEGUNDA',detritment_cents: '308000000.0',fail_value_cents: '308000000.0',office_name: 'JUZGADO 1 ADMINISTRATIVO CIRCUITO DE MEDELLIN',internal_lawyer: '1',state: 'REGISTRO MIGRADO')
+
+TypeProcess.create(p_type: 2,process_radicate: '20140049000',case_id_bap: 'GPJ-2700-2016',departament: '76',city_case: '001',passive_part: ' ',instance: ' ',detritment_cents: 'PENDIENTE',fail_value_cents: 'PENDIENTE',office_name: 'JUZGADO 10 ADMINISTRATIVO ORALES DE CALI',internal_lawyer: '1',state: 'REGISTRO MIGRADO')
+
+TypeProcess.create(p_type: 2,process_radicate: '20140062200',case_id_bap: 'GPJ-2706-2016',departament: '41',city_case: '001',passive_part: ' ',instance: ' ',detritment_cents: 'PENDIENTE',fail_value_cents: 'PENDIENTE',office_name: 'JUZGADO 1 LABORAL CIRCUITO DE NEIVA',internal_lawyer: '1',state: 'REGISTRO MIGRADO')
+
+TypeProcess.create(p_type: 2,process_radicate: '20110021600',case_id_bap: 'GPJ-2712-2016',departament: '11',city_case: '001',passive_part: ' ',instance: ' ',detritment_cents: 'PENDIENTE',fail_value_cents: 'PENDIENTE',office_name: 'JUZGADO 60 ADMINISTRATIVO CIRCUITO DE BOGOTA',internal_lawyer: '1',state: 'REGISTRO MIGRADO')
+
+TypeProcess.create(p_type: 2,process_radicate: '20150001200',case_id_bap: 'GPJ-3546-2016',departament: '11',city_case: '001',passive_part: ' ',instance: ' ',detritment_cents: 'PENDIENTE',fail_value_cents: 'PENDIENTE',office_name: 'JUZGADO 2 ADMINISTRATIVO CIRCUITO DE BOGOTA',internal_lawyer: '1',state: 'REGISTRO MIGRADO')
+
+TypeProcess.create(p_type: 2,process_radicate: '20140068800',case_id_bap: 'GPJ-3592-2016',departament: '05',city_case: '001',passive_part: ' ',instance: ' ',detritment_cents: 'PENDIENTE',fail_value_cents: 'PENDIENTE',office_name: 'JUZGADO 11 ADMINISTRATIVO CIRCUITO DE MEDELLIN',internal_lawyer: '1',state: 'REGISTRO MIGRADO')
+
+TypeProcess.create(p_type: 2,process_radicate: ' ',case_id_bap: 'GPJ-3852-2016',departament: '05',city_case: '001',passive_part: ' ',instance: ' ',detritment_cents: 'PENDIENTE',fail_value_cents: 'PENDIENTE',office_name: 'TRIBUNAL ADMINISTRATIVO',internal_lawyer: '1',state: 'REGISTRO MIGRADO')
+
+TypeProcess.create(p_type: 2,process_radicate: '20140006600',case_id_bap: 'GPJ-4846-2016',departament: '11',city_case: '001',passive_part: ' ',instance: ' ',detritment_cents: 'PENDIENTE',fail_value_cents: 'PENDIENTE',office_name: 'JUZGADO 45 ADMINISTRATIVO CIRCUITO DE BOGOTA',internal_lawyer: '1',state: 'REGISTRO MIGRADO')
+
+TypeProcess.create(p_type: 2,process_radicate: '20130006200',case_id_bap: 'GPJ-4905-2016',departament: '86',city_case: '001',passive_part: ' ',instance: ' ',detritment_cents: 'PENDIENTE',fail_value_cents: 'PENDIENTE',office_name: 'JUZGADO 2 ADMINISTRATIVO CIRCUITO DE MOCOA',internal_lawyer: '1',state: 'REGISTRO MIGRADO')
+
+TypeProcess.create(p_type: 2,process_radicate: '20110069400',case_id_bap: 'GPJ-5154-2016',departament: '05',city_case: '001',passive_part: ' ',instance: ' ',detritment_cents: 'PENDIENTE',fail_value_cents: 'PENDIENTE',office_name: 'JUZGADO 32 ADMINISTRATIVO CIRCUITO DE MEDELLIN',internal_lawyer: '1',state: 'REGISTRO MIGRADO')
+
+TypeProcess.create(p_type: 2,process_radicate: '20130047700',case_id_bap: 'GPJ-5472-2016',departament: '54',city_case: '001',passive_part: ' ',instance: ' ',detritment_cents: 'PENDIENTE',fail_value_cents: 'PENDIENTE',office_name: 'JUZGADO 2 ADMINISTRATIVO CIRCUITO DE CUCUTA',internal_lawyer: '1',state: 'REGISTRO MIGRADO')
+
+TypeProcess.create(p_type: 2,process_radicate: '20150007700',case_id_bap: 'GPJ-5503-2016',departament: '05',city_case: '001',passive_part: ' ',instance: ' ',detritment_cents: 'PENDIENTE',fail_value_cents: 'PENDIENTE',office_name: 'JUZGADO 1 ADMINISTRATIVO CIRCUITO DE MEDELLIN',internal_lawyer: '1',state: 'REGISTRO MIGRADO')
+
+TypeProcess.create(p_type: 2,process_radicate: '20150123100',case_id_bap: 'GPJ-6222-2016',departament: '11',city_case: '001',passive_part: ' ',instance: ' ',detritment_cents: 'PENDIENTE',fail_value_cents: 'PENDIENTE',office_name: 'JUZGADO 31 CIVIL CIRCUITO DE BOGOTA',internal_lawyer: '1',state: 'REGISTRO MIGRADO')
+
+TypeProcess.create(p_type: 2,process_radicate: '20130027400',case_id_bap: 'GPJ-6465-2016',departament: '11',city_case: '001',passive_part: ' ',instance: ' ',detritment_cents: 'PENDIENTE',fail_value_cents: 'PENDIENTE',office_name: 'JUZGADO 61 ADMINISTRATIVO CIRCUITO DE BOGOTA',internal_lawyer: '1',state: 'REGISTRO MIGRADO')
+
+TypeProcess.create(p_type: 2,process_radicate: '20100003500',case_id_bap: 'GPJ-6469-2016',departament: '11',city_case: '001',passive_part: 'PENDIENTE',instance: 'PENDIENTE',detritment_cents: 'PENDIENTE',fail_value_cents: 'PENDIENTE',office_name: 'JUZGADO 60 ADMINISTRATIVO CIRCUITO DE BOGOTA',internal_lawyer: '1',state: 'REGISTRO MIGRADO')
+  end
+end
