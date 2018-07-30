618c244ee9f9ccecde0d416d60115124aa0da0a6
joagranadosme <joagranadosme@unal.edu.co>
2018-07-30 15:01:26 -0500

    Updating attornies

--- app/models/city_case.rb
+++ app/models/city_case.rb
@@ -2,10 +2,12 @@
 #
 # Table name: city_cases
 #
-#  id         :integer          not null, primary key
-#  name       :string
-#  created_at :datetime         not null
-#  updated_at :datetime         not null
+#  id          :integer          not null, primary key
+#  name        :string
+#  created_at  :datetime         not null
+#  updated_at  :datetime         not null
+#  code        :string
+#  departament :string
 #
 
 class CityCase < ApplicationRecord
