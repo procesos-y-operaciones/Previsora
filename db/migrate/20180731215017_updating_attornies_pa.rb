c06a4a5c39168c05340a1764e9f1d31007a1fc59
joagranadosme <joagranadosme@unal.edu.co>
2018-07-31 16:53:06 -0500

    Updating attornies PA

---
+++ db/migrate/20180731215017_updating_attornies_pa.rb
@@ -0,0 +1,563 @@
+class UpdatingAttorniesPa < ActiveRecord::Migration[5.1]
+  def change
+    TypeProcess.find(19807).update(attorny: 2)
+
+    TypeProcess.find(19777).update(attorny: 3)
+
+    TypeProcess.find(19784).update(attorny: 3)
+
+    TypeProcess.find(19804).update(attorny: 3)
+
+    TypeProcess.find(19817).update(attorny: 3)
+
+    TypeProcess.find(19819).update(attorny: 3)
+
+    TypeProcess.find(19833).update(attorny: 3)
+
+    TypeProcess.find(19839).update(attorny: 3)
+
+    TypeProcess.find(19853).update(attorny: 3)
+
+    TypeProcess.find(19858).update(attorny: 3)
+
+    TypeProcess.find(19866).update(attorny: 3)
+
+    TypeProcess.find(19884).update(attorny: 3)
+
+    TypeProcess.find(19887).update(attorny: 3)
+
+    TypeProcess.find(19837).update(attorny: 45)
+
+    TypeProcess.find(19775).update(attorny: 53)
+
+    TypeProcess.find(19792).update(attorny: 53)
+
+    TypeProcess.find(19793).update(attorny: 53)
+
+    TypeProcess.find(19794).update(attorny: 53)
+
+    TypeProcess.find(19796).update(attorny: 53)
+
+    TypeProcess.find(19798).update(attorny: 53)
+
+    TypeProcess.find(19799).update(attorny: 53)
+
+    TypeProcess.find(19813).update(attorny: 53)
+
+    TypeProcess.find(19815).update(attorny: 53)
+
+    TypeProcess.find(19818).update(attorny: 53)
+
+    TypeProcess.find(19825).update(attorny: 53)
+
+    TypeProcess.find(19832).update(attorny: 53)
+
+    TypeProcess.find(19843).update(attorny: 53)
+
+    TypeProcess.find(19852).update(attorny: 53)
+
+    TypeProcess.find(19854).update(attorny: 53)
+
+    TypeProcess.find(19860).update(attorny: 53)
+
+    TypeProcess.find(19868).update(attorny: 53)
+
+    TypeProcess.find(19891).update(attorny: 53)
+
+    TypeProcess.find(19892).update(attorny: 53)
+
+    TypeProcess.find(19896).update(attorny: 53)
+
+    TypeProcess.find(19897).update(attorny: 53)
+
+    TypeProcess.find(19898).update(attorny: 53)
+
+    TypeProcess.find(19904).update(attorny: 53)
+
+    TypeProcess.find(19907).update(attorny: 53)
+
+    TypeProcess.find(19913).update(attorny: 53)
+
+    TypeProcess.find(19914).update(attorny: 53)
+
+    TypeProcess.find(19915).update(attorny: 53)
+
+    TypeProcess.find(19917).update(attorny: 53)
+
+    TypeProcess.find(19924).update(attorny: 53)
+
+    TypeProcess.find(19925).update(attorny: 53)
+
+    TypeProcess.find(19926).update(attorny: 53)
+
+    TypeProcess.find(19927).update(attorny: 53)
+
+    TypeProcess.find(19928).update(attorny: 53)
+
+    TypeProcess.find(19929).update(attorny: 53)
+
+    TypeProcess.find(19933).update(attorny: 53)
+
+    TypeProcess.find(19934).update(attorny: 53)
+
+    TypeProcess.find(19935).update(attorny: 53)
+
+    TypeProcess.find(19936).update(attorny: 53)
+
+    TypeProcess.find(19937).update(attorny: 53)
+
+    TypeProcess.find(19940).update(attorny: 53)
+
+    TypeProcess.find(19941).update(attorny: 53)
+
+    TypeProcess.find(19946).update(attorny: 53)
+
+    TypeProcess.find(19947).update(attorny: 53)
+
+    TypeProcess.find(19948).update(attorny: 53)
+
+    TypeProcess.find(19949).update(attorny: 53)
+
+    TypeProcess.find(19950).update(attorny: 53)
+
+    TypeProcess.find(19951).update(attorny: 53)
+
+    TypeProcess.find(19952).update(attorny: 53)
+
+    TypeProcess.find(19953).update(attorny: 53)
+
+    TypeProcess.find(19954).update(attorny: 53)
+
+    TypeProcess.find(19955).update(attorny: 53)
+
+    TypeProcess.find(19956).update(attorny: 53)
+
+    TypeProcess.find(19957).update(attorny: 53)
+
+    TypeProcess.find(19958).update(attorny: 53)
+
+    TypeProcess.find(19959).update(attorny: 53)
+
+    TypeProcess.find(19960).update(attorny: 53)
+
+    TypeProcess.find(19961).update(attorny: 53)
+
+    TypeProcess.find(19964).update(attorny: 53)
+
+    TypeProcess.find(19965).update(attorny: 53)
+
+    TypeProcess.find(19966).update(attorny: 53)
+
+    TypeProcess.find(19970).update(attorny: 53)
+
+    TypeProcess.find(19972).update(attorny: 53)
+
+    TypeProcess.find(19973).update(attorny: 53)
+
+    TypeProcess.find(19974).update(attorny: 53)
+
+    TypeProcess.find(19975).update(attorny: 53)
+
+    TypeProcess.find(19976).update(attorny: 53)
+
+    TypeProcess.find(19977).update(attorny: 53)
+
+    TypeProcess.find(19978).update(attorny: 53)
+
+    TypeProcess.find(19979).update(attorny: 53)
+
+    TypeProcess.find(19980).update(attorny: 53)
+
+    TypeProcess.find(19981).update(attorny: 53)
+
+    TypeProcess.find(19982).update(attorny: 53)
+
+    TypeProcess.find(19983).update(attorny: 53)
+
+    TypeProcess.find(19984).update(attorny: 53)
+
+    TypeProcess.find(19985).update(attorny: 53)
+
+    TypeProcess.find(19986).update(attorny: 53)
+
+    TypeProcess.find(19987).update(attorny: 53)
+
+    TypeProcess.find(19988).update(attorny: 53)
+
+    TypeProcess.find(19989).update(attorny: 53)
+
+    TypeProcess.find(19990).update(attorny: 53)
+
+    TypeProcess.find(19991).update(attorny: 53)
+
+    TypeProcess.find(19992).update(attorny: 53)
+
+    TypeProcess.find(19993).update(attorny: 53)
+
+    TypeProcess.find(19994).update(attorny: 53)
+
+    TypeProcess.find(19995).update(attorny: 53)
+
+    TypeProcess.find(19996).update(attorny: 53)
+
+    TypeProcess.find(19105).update(attorny: 54)
+
+    TypeProcess.find(19157).update(attorny: 54)
+
+    TypeProcess.find(19158).update(attorny: 54)
+
+    TypeProcess.find(19159).update(attorny: 54)
+
+    TypeProcess.find(19160).update(attorny: 54)
+
+    TypeProcess.find(19161).update(attorny: 54)
+
+    TypeProcess.find(19162).update(attorny: 54)
+
+    TypeProcess.find(19163).update(attorny: 54)
+
+    TypeProcess.find(19164).update(attorny: 54)
+
+    TypeProcess.find(19165).update(attorny: 54)
+
+    TypeProcess.find(19166).update(attorny: 54)
+
+    TypeProcess.find(19167).update(attorny: 54)
+
+    TypeProcess.find(19168).update(attorny: 54)
+
+    TypeProcess.find(19169).update(attorny: 54)
+
+    TypeProcess.find(19170).update(attorny: 54)
+
+    TypeProcess.find(19171).update(attorny: 54)
+
+    TypeProcess.find(19172).update(attorny: 54)
+
+    TypeProcess.find(19173).update(attorny: 54)
+
+    TypeProcess.find(19174).update(attorny: 54)
+
+    TypeProcess.find(19175).update(attorny: 54)
+
+    TypeProcess.find(19176).update(attorny: 54)
+
+    TypeProcess.find(19177).update(attorny: 54)
+
+    TypeProcess.find(19178).update(attorny: 54)
+
+    TypeProcess.find(19179).update(attorny: 54)
+
+    TypeProcess.find(19180).update(attorny: 54)
+
+    TypeProcess.find(19181).update(attorny: 54)
+
+    TypeProcess.find(19182).update(attorny: 54)
+
+    TypeProcess.find(19183).update(attorny: 54)
+
+    TypeProcess.find(19184).update(attorny: 54)
+
+    TypeProcess.find(19185).update(attorny: 54)
+
+    TypeProcess.find(19186).update(attorny: 54)
+
+    TypeProcess.find(19187).update(attorny: 54)
+
+    TypeProcess.find(19188).update(attorny: 54)
+
+    TypeProcess.find(19189).update(attorny: 54)
+
+    TypeProcess.find(19190).update(attorny: 54)
+
+    TypeProcess.find(19191).update(attorny: 54)
+
+    TypeProcess.find(19192).update(attorny: 54)
+
+    TypeProcess.find(19193).update(attorny: 54)
+
+    TypeProcess.find(19194).update(attorny: 54)
+
+    TypeProcess.find(19195).update(attorny: 54)
+
+    TypeProcess.find(19196).update(attorny: 54)
+
+    TypeProcess.find(19197).update(attorny: 54)
+
+    TypeProcess.find(19198).update(attorny: 54)
+
+    TypeProcess.find(19199).update(attorny: 54)
+
+    TypeProcess.find(19200).update(attorny: 54)
+
+    TypeProcess.find(19201).update(attorny: 54)
+
+    TypeProcess.find(19202).update(attorny: 54)
+
+    TypeProcess.find(19203).update(attorny: 54)
+
+    TypeProcess.find(19204).update(attorny: 54)
+
+    TypeProcess.find(19205).update(attorny: 54)
+
+    TypeProcess.find(19206).update(attorny: 54)
+
+    TypeProcess.find(19207).update(attorny: 54)
+
+    TypeProcess.find(19208).update(attorny: 54)
+
+    TypeProcess.find(19209).update(attorny: 54)
+
+    TypeProcess.find(19210).update(attorny: 54)
+
+    TypeProcess.find(19211).update(attorny: 54)
+
+    TypeProcess.find(19212).update(attorny: 54)
+
+    TypeProcess.find(19213).update(attorny: 54)
+
+    TypeProcess.find(19214).update(attorny: 54)
+
+    TypeProcess.find(19215).update(attorny: 54)
+
+    TypeProcess.find(19216).update(attorny: 54)
+
+    TypeProcess.find(19217).update(attorny: 54)
+
+    TypeProcess.find(19218).update(attorny: 54)
+
+    TypeProcess.find(19830).update(attorny: 60)
+
+    TypeProcess.find(19831).update(attorny: 60)
+
+    TypeProcess.find(19859).update(attorny: 60)
+
+    TypeProcess.find(19922).update(attorny: 60)
+
+    TypeProcess.find(19903).update(attorny: 77)
+
+    TypeProcess.find(19776).update(attorny: 82)
+
+    TypeProcess.find(19778).update(attorny: 82)
+
+    TypeProcess.find(19779).update(attorny: 82)
+
+    TypeProcess.find(19781).update(attorny: 82)
+
+    TypeProcess.find(19782).update(attorny: 82)
+
+    TypeProcess.find(19783).update(attorny: 82)
+
+    TypeProcess.find(19785).update(attorny: 82)
+
+    TypeProcess.find(19786).update(attorny: 82)
+
+    TypeProcess.find(19787).update(attorny: 82)
+
+    TypeProcess.find(19788).update(attorny: 82)
+
+    TypeProcess.find(19789).update(attorny: 82)
+
+    TypeProcess.find(19791).update(attorny: 82)
+
+    TypeProcess.find(19795).update(attorny: 82)
+
+    TypeProcess.find(19797).update(attorny: 82)
+
+    TypeProcess.find(19800).update(attorny: 82)
+
+    TypeProcess.find(19802).update(attorny: 82)
+
+    TypeProcess.find(19803).update(attorny: 82)
+
+    TypeProcess.find(19805).update(attorny: 82)
+
+    TypeProcess.find(19806).update(attorny: 82)
+
+    TypeProcess.find(19808).update(attorny: 82)
+
+    TypeProcess.find(19809).update(attorny: 82)
+
+    TypeProcess.find(19814).update(attorny: 82)
+
+    TypeProcess.find(19820).update(attorny: 82)
+
+    TypeProcess.find(19821).update(attorny: 82)
+
+    TypeProcess.find(19822).update(attorny: 82)
+
+    TypeProcess.find(19823).update(attorny: 82)
+
+    TypeProcess.find(19824).update(attorny: 82)
+
+    TypeProcess.find(19827).update(attorny: 82)
+
+    TypeProcess.find(19834).update(attorny: 82)
+
+    TypeProcess.find(19838).update(attorny: 82)
+
+    TypeProcess.find(19840).update(attorny: 82)
+
+    TypeProcess.find(19841).update(attorny: 82)
+
+    TypeProcess.find(19842).update(attorny: 82)
+
+    TypeProcess.find(19844).update(attorny: 82)
+
+    TypeProcess.find(19845).update(attorny: 82)
+
+    TypeProcess.find(19846).update(attorny: 82)
+
+    TypeProcess.find(19847).update(attorny: 82)
+
+    TypeProcess.find(19848).update(attorny: 82)
+
+    TypeProcess.find(19849).update(attorny: 82)
+
+    TypeProcess.find(19850).update(attorny: 82)
+
+    TypeProcess.find(19851).update(attorny: 82)
+
+    TypeProcess.find(19861).update(attorny: 82)
+
+    TypeProcess.find(19862).update(attorny: 82)
+
+    TypeProcess.find(19863).update(attorny: 82)
+
+    TypeProcess.find(19864).update(attorny: 82)
+
+    TypeProcess.find(19865).update(attorny: 82)
+
+    TypeProcess.find(19867).update(attorny: 82)
+
+    TypeProcess.find(19875).update(attorny: 82)
+
+    TypeProcess.find(19876).update(attorny: 82)
+
+    TypeProcess.find(19881).update(attorny: 82)
+
+    TypeProcess.find(19885).update(attorny: 82)
+
+    TypeProcess.find(19886).update(attorny: 82)
+
+    TypeProcess.find(19889).update(attorny: 82)
+
+    TypeProcess.find(19902).update(attorny: 82)
+
+    TypeProcess.find(19910).update(attorny: 82)
+
+    TypeProcess.find(19918).update(attorny: 82)
+
+    TypeProcess.find(19919).update(attorny: 82)
+
+    TypeProcess.find(19920).update(attorny: 82)
+
+    TypeProcess.find(19921).update(attorny: 82)
+
+    TypeProcess.find(19938).update(attorny: 82)
+
+    TypeProcess.find(19939).update(attorny: 82)
+
+    TypeProcess.find(19801).update(attorny: 88)
+
+    TypeProcess.find(19810).update(attorny: 88)
+
+    TypeProcess.find(19816).update(attorny: 88)
+
+    TypeProcess.find(19826).update(attorny: 88)
+
+    TypeProcess.find(19829).update(attorny: 88)
+
+    TypeProcess.find(19836).update(attorny: 88)
+
+    TypeProcess.find(19855).update(attorny: 88)
+
+    TypeProcess.find(19895).update(attorny: 88)
+
+    TypeProcess.find(19905).update(attorny: 88)
+
+    TypeProcess.find(19906).update(attorny: 91)
+
+    TypeProcess.find(19962).update(attorny: 101)
+
+    TypeProcess.find(19963).update(attorny: 101)
+
+    TypeProcess.find(19890).update(attorny: 111)
+
+    TypeProcess.find(19911).update(attorny: 111)
+
+    TypeProcess.find(19835).update(attorny: 120)
+
+    TypeProcess.find(19780).update(attorny: 121)
+
+    TypeProcess.find(19811).update(attorny: 121)
+
+    TypeProcess.find(19828).update(attorny: 121)
+
+    TypeProcess.find(19856).update(attorny: 121)
+
+    TypeProcess.find(19857).update(attorny: 121)
+
+    TypeProcess.find(19869).update(attorny: 121)
+
+    TypeProcess.find(19870).update(attorny: 121)
+
+    TypeProcess.find(19871).update(attorny: 121)
+
+    TypeProcess.find(19872).update(attorny: 121)
+
+    TypeProcess.find(19873).update(attorny: 121)
+
+    TypeProcess.find(19874).update(attorny: 121)
+
+    TypeProcess.find(19877).update(attorny: 121)
+
+    TypeProcess.find(19878).update(attorny: 121)
+
+    TypeProcess.find(19880).update(attorny: 121)
+
+    TypeProcess.find(19882).update(attorny: 121)
+
+    TypeProcess.find(19893).update(attorny: 121)
+
+    TypeProcess.find(19894).update(attorny: 121)
+
+    TypeProcess.find(19899).update(attorny: 121)
+
+    TypeProcess.find(19900).update(attorny: 121)
+
+    TypeProcess.find(19901).update(attorny: 121)
+
+    TypeProcess.find(19908).update(attorny: 121)
+
+    TypeProcess.find(19909).update(attorny: 121)
+
+    TypeProcess.find(19912).update(attorny: 121)
+
+    TypeProcess.find(19916).update(attorny: 121)
+
+    TypeProcess.find(19923).update(attorny: 121)
+
+    TypeProcess.find(19930).update(attorny: 121)
+
+    TypeProcess.find(19931).update(attorny: 121)
+
+    TypeProcess.find(19932).update(attorny: 121)
+
+    TypeProcess.find(19943).update(attorny: 121)
+
+    TypeProcess.find(19944).update(attorny: 121)
+
+    TypeProcess.find(19945).update(attorny: 121)
+
+    TypeProcess.find(19967).update(attorny: 121)
+
+    TypeProcess.find(19968).update(attorny: 121)
+
+    TypeProcess.find(19969).update(attorny: 121)
+
+    TypeProcess.find(19971).update(attorny: 121)
+
+    TypeProcess.find(19883).update(attorny: 137)
+  end
+end
