30b31cb009a6e54a96f1babbe1abdb2f84d0a26b
joagranadosme <joagranadosme@unal.edu.co>
2018-08-02 14:52:34 -0500

    :

--- db/migrate/20180710204537_change_type_processes_attorny.rb
+++ db/migrate/20180710204537_change_type_processes_attorny.rb
@@ -2,7 +2,7 @@ class ChangeTypeProcessesAttorny < ActiveRecord::Migration[5.1]
   def change
 
     remove_column :type_processes, :attorny,
-    add_column :type_processes, :attorny,, :integer
+    add_column :type_processes, :attorny, :integer
 
     #change_column :type_processes, :attorny, :integer
   end
