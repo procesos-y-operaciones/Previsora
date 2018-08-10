09cb9412e9db6118e00855520691ea450bfacc4c
joagranadosme <joagranadosme@unal.edu.co>
2018-08-09 19:19:38 -0500

    Adding web service & fixing offices and departaments

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -7,9 +7,16 @@ class ApplicationController < ActionController::Base
   #before_action :configure_permitted_parameters, if: :devise_controller?
 
   def cities
-    #render json: CS.cities(params[:state], :co).to_json
     render json: CityCase.select(:id, :code, :name).order(name: :asc).where(departament: params[:state]).to_json
   end
+
+  def offices
+    @parameters = params[:name].split(",")
+    render json: OfficeName.select(:id, :name).order(name: :asc).where(
+      "name like ? and name like ? and name like ? or name like ? and name like ? and name like ?",
+      "%#{@parameters[0]}%", "%#{@parameters[2]}%", "%#{@parameters[3]}%", "%#{@parameters[1]}%", "%#{@parameters[2]}%", "%#{@parameters[3]}%",
+    ).to_json
+end
 
   protected
     def configure_permitted_parameters
