266ed1854ca3d993791adfd94050831646d01092
joagranadosme <joagranadosme@unal.edu.co>
2018-08-14 12:39:26 -0500

    Adding migration

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180810144428) do
+ActiveRecord::Schema.define(version: 20180814162203) do
 
   create_table "attornies", force: :cascade do |t|
     t.string "name"
