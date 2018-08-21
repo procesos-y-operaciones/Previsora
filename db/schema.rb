9336e5acb35a16501cfea16de507fac49cb6d8e0
joagranadosme <joagranadosme@unal.edu.co>
2018-08-21 10:48:55 -0500

    Forcing city & departament

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180814191610) do
+ActiveRecord::Schema.define(version: 20180809164009) do
 
   create_table "attornies", force: :cascade do |t|
     t.string "name"
