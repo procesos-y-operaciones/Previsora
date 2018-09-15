964f1f0548c9978c5d7612ff9c9172c58df3e7f1
joagranadosme <joagranadosme@unal.edu.co>
2018-09-15 17:35:11 -0500

    Fixing creating

--- db/migrate/20180915215208_aleja44.rb
+++
@@ -1,7 +0,0 @@
-class Aleja44 < ActiveRecord::Migration[5.1]
-  def change
-    puts TypeProcess.find(5344).update(id: 5344,recovery: true,state: 'REGISTRO MIGRADO SOBRESCRITO')
-    puts TypeProcess.find(4528).update(id: 4528,recovery: true,state: 'REGISTRO MIGRADO SOBRESCRITO')
-    puts TypeProcess.find(3453).update(id: 3453,recovery: true,state: 'REGISTRO MIGRADO SOBRESCRITO')
-  end
-end
