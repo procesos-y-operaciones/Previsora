8d427691f48b6c8c7d47275ee65c565b5b6b09dc
joagranadosme <joagranadosme@unal.edu.co>
2018-09-15 18:01:12 -0500

    Adding more migrations

---
+++ db/migrate/20180915225826_aleja74.rb
@@ -0,0 +1,6 @@
+class Aleja74 < ActiveRecord::Migration[5.1]
+  def change
+    puts TypeProcess.find(9981).update(id: 9981,reason_inv: 'PENDIENTE EL FALLO DE PRIMERA INSTANCIA',state: 'REGISTRO NUEVO ACTUALIZADO')
+    puts TypeProcess.find(9423).update(id: 9423,reason_inv: 'NO SE ACREDITO DE FORMA PROBATORIA O CIENTIFICA QUE EXISTIO MALA PRAXIS POR PARTE DE LA ENTIDAD ASEGURADA',state: 'REGISTRO NUEVO ACTUALIZADO')
+  end
+end
