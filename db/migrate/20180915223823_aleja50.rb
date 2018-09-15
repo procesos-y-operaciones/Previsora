91b0c56e6e1256b46e21316525447fb9f28e2da9
joagranadosme <joagranadosme@unal.edu.co>
2018-09-15 17:47:08 -0500

    Adding migrations

---
+++ db/migrate/20180915223823_aleja50.rb
@@ -0,0 +1,12 @@
+class Aleja50 < ActiveRecord::Migration[5.1]
+  def change
+    puts TypeProcess.find(5265).update(id: 5265,case_id_sise: '201010004550',state: 'REGISTRO NUEVO ACTUALIZADO')
+    puts TypeProcess.find(5639).update(id: 5639,case_id_sise: '200710001731',state: 'REGISTRO NUEVO ACTUALIZADO')
+    puts TypeProcess.find(5460).update(id: 5460,case_id_sise: '201110003039',state: 'REGISTRO NUEVO ACTUALIZADO')
+    puts TypeProcess.find(5929).update(id: 5929,case_id_sise: '201110002729',state: 'REGISTRO NUEVO ACTUALIZADO')
+    puts TypeProcess.find(5576).update(id: 5576,case_id_sise: '201410004225',state: 'REGISTRO NUEVO ACTUALIZADO')
+    puts TypeProcess.find(5682).update(id: 5682,case_id_sise: '201310004138',state: 'REGISTRO NUEVO ACTUALIZADO')
+    puts TypeProcess.find(5262).update(id: 5262,case_id_sise: '201410004344',state: 'REGISTRO NUEVO ACTUALIZADO')
+    puts TypeProcess.find(4704).update(id: 4704,case_id_sise: '201403000684',state: 'REGISTRO NUEVO ACTUALIZADO')
+  end
+end
