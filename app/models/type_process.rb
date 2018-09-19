62624e78c469e2df0f49d67431f67f09e8bb52e4
joagranadosme <joagranadosme@unal.edu.co>
2018-09-19 13:07:57 -0500

    Adding date created

--- app/models/type_process.rb
+++ app/models/type_process.rb
@@ -207,7 +207,7 @@ class TypeProcess < ApplicationRecord
       'FECHA DE LA NOTIFICACION DE LA DECISION O FALLO', 'FECHA INGRESO AL COMITE DE DEFENSA Y CONCILIACION',	'FECHA DE NOTIFICACION DEL INCIDENTE DEL DESACATO',
       'FECHA DE CONTESTACION DEL INCIDENTE DE DESACATO', 'FECHA NOTIFICACION DE DECISION DEL INCIDENTE DE DESACATO', 'REASEGURO REPORTADO',
       'PROCEDE RECOBRO?',	'TIENE NUMERO DE RADICADO DE CORRESPONDENCIA / VICEPRESIDENCIA?',	'TIENE MAS POLIZAS?',	'TIENE COSTAS A FAVOR?',	'TIENE NUMERO DE RADICADO DE COACTIVO?',
-      'HAY IMPUGNACION?',	'HAY DESACATO?', 'OTRO DESPACHO', 'ZONA', 'ULTIMA ACTUALIZACION', 'OBSERVACIONES'
+      'HAY IMPUGNACION?',	'HAY DESACATO?', 'OTRO DESPACHO', 'ZONA', 'ULTIMA ACTUALIZACION', 'FECHA DE CREACION','OBSERVACIONES'
     ]
   end
 
@@ -223,7 +223,7 @@ class TypeProcess < ApplicationRecord
       nilValue(self.reconcilie_value_cents), nilValue(self.cost_value_cents), self.creation_date, format_date(self.notification_date), format_date(self.attorny_date), format_date(self.failed_notification_date), format_date(self.answer_date), format_date(self.objection_date_desition),
       format_date(self.date_notification_desition_desacate), format_date(self.payment_date), format_date(self.imp_date),format_date(self.last_performance_date), format_date(self.desition_date), format_date(self.committee_date), format_date(self.date_notification_desacate),
       format_date(self.date_answer_desacate), format_date(self.objection_date_desition_notification), self.get_reinsurance_report, self.get_recovery, booleanValue(self.has_correspondency_radicate),
-      booleanValue(self.has_more_polcies), booleanValue(self.has_costs), booleanValue(self.has_coactive_radicate), booleanValue(self.has_impug), booleanValue(self.has_desacate), self.other_office_name, self.get_zone, (self.updated_at-5.hour).strftime("%d/%m/%Y %H:%M"), self.observations
+      booleanValue(self.has_more_polcies), booleanValue(self.has_costs), booleanValue(self.has_coactive_radicate), booleanValue(self.has_impug), booleanValue(self.has_desacate), self.other_office_name, self.get_zone, (self.updated_at).strftime("%d/%m/%Y %H:%M"), (self.created_at).strftime("%d/%m/%Y %H:%M"),self.observations
     ]
   end
 
