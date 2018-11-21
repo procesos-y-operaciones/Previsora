65a86faa6af56e12238a8d568f3b657c861da16c
joagranadosme <joagranadosme@unal.edu.co>
2018-11-21 12:43:28 -0500

    .

--- config/initializers/locale.rb
+++ config/initializers/locale.rb
@@ -7,9 +7,3 @@ I18n.available_locales = [:en, :'es-MX']
 
 # Set default locale to something other than :en
 I18n.default_locale = :'es-MX'
-
-#Active Job
-day = 7 - (Time.now.day % 7)
-hour = 23 - (Time.now.hour % 23)
-BackupJob.set(wait: day.day + hour.hour).perform_later
-puts "------> BackupJob will be executed in #{day} days and #{hour} hours. <------"
