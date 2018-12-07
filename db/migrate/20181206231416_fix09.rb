79dc29cb06c1fd4b6f0f83b48f692dcac7b3035f
joagranadosme <joagranadosme@unal.edu.co>
2018-12-06 19:04:40 -0500

    .

--- db/migrate/20181206231416_fix09.rb
+++ db/migrate/20181206231416_fix09.rb
@@ -1,5 +1,5 @@
 class Fix09 < ActiveRecord::Migration[5.1]
   def change
-    puts TypeProcess.find(22879).update(document_active_part: 'PENDIENTE',document_pasive_part: 'PENDIENTE',reinsurance_type: 'PENDIENTE',last_performance: 'PENDIENTE',reserved_released: 'PENDIENTE',money_type: 'PENDIENTE',coensurance_type: 'PENDIENTE',state_db: 'REGISTRO MIGRADO SOBRESCRITO')
+    puts TypeProcess.find(22879).update(document_active_part: 'PENDIENTE',document_passive_part: 'PENDIENTE',reinsurance_type: 'PENDIENTE',last_performance: 'PENDIENTE',reserved_released: 'PENDIENTE',money_type: 'PENDIENTE',coensurance_type: 'PENDIENTE',state_db: 'REGISTRO MIGRADO SOBRESCRITO')
   end
 end
